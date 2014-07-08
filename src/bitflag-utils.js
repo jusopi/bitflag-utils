459798c4f640a68093ec1f2e313490d4a43db04f
Justin Opitz <justin.opitz@neohapsis.com>
2014-07-08 17:01:41 -0500

    added apis

--- src/bitflag-utils.js
+++ src/bitflag-utils.js
@@ -1,5 +1,23 @@
 /**
- * Created by jopitz on 7/8/2014.
+ * Created by jopitz on 1/8/2013.
  */
 "use strict";
 
+var api = {
+
+    hasFlags: function( flagsToCheckFor, currentFlags )
+    {
+        return flagsToCheckFor == (currentFlags & flagsToCheckFor);
+    },
+
+    addFlags: function( toAdd, currentFlags )
+    {
+        return toAdd | currentFlags;
+    },
+
+    delFlags: function( toDel, currentFlags )
+    {
+        return currentFlags & ~toDel;
+    }
+
+};
